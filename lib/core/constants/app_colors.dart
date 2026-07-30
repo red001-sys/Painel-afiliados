135870329b79ff41383694b756f348aab199aef2
red001-sys <redstarstore0@gmail.com>
2026-07-30 18:17:56 +0000

    att 2

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColors {
-  // EcoFlow Green Palette
-  static const Color ecoGreen = Color(0xFF00B140);
-  static const Color ecoGreenLight = Color(0xFF4CD964);
-  static const Color ecoGreenDark = Color(0xFF008C2E);
-  static const Color ecoGreenSurface = Color(0xFFE8F5E9);
+  // Blue Palette
+  static const Color ecoGreen = Color(0xFF2196F3);
+  static const Color ecoGreenLight = Color(0xFF64B5F6);
+  static const Color ecoGreenDark = Color(0xFF1565C0);
+  static const Color ecoGreenSurface = Color(0xFFE3F2FD);
 
   // Neutrals
   static const Color neutral50 = Color(0xFFFAFAFA);
@@ -20,7 +20,7 @@ abstract final class AppColors {
   static const Color neutral900 = Color(0xFF212121);
 
   // Semantic
-  static const Color success = Color(0xFF00B140);
+  static const Color success = Color(0xFF2196F3);
   static const Color error = Color(0xFFE53935);
   static const Color warning = Color(0xFFFF9800);
   static const Color info = Color(0xFF2196F3);
